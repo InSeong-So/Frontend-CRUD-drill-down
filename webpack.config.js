8255cac0d55a45d4136d0d2431a15f9951a34f72
InSeongSo <goflvhxj2547@gmail.com>
2022-01-16 23:43:18 +0900

    🔬Chore: cypress 설정

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,12 @@ module.exports = {
     open: true,
     historyApiFallback: true,
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   target: ['es5', 'web'],
-  entry: './index.ts',
+  entry: './src/index.ts',
   output: {
-    publicPath: '/',
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: '.',
+    path: path.resolve(__dirname, 'public'),
     filename: '[chunkhash].js',
     clean: true,
   },
@@ -32,7 +32,7 @@ module.exports = {
       favicon: './assets/icon/favicon.ico',
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    // new MiniCssExtractPlugin({ filename: 'app.css' }),
   ],
   module: {
     rules: [
@@ -53,13 +53,16 @@ module.exports = {
       },
       {
         test: /\.(ts|tsx)$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /(node_modules)|(dist)/,
         use: {
           loader: 'ts-loader',
           options: {
             configFile: path.resolve(__dirname, './tsconfig.json'),
+            // skip typechecking for speed
+            transpileOnly: true,
           },
         },
-        exclude: /node_modules/,
       },
     ],
   },
